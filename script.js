85a5fe61958ddfbb4d509536a19f4fa2d4a53878
Daniel Kaplun <kdd@gosniias.ru>
2022-12-09 21:16:31 +0300

    fixed header

--- script.js
+++ script.js
@@ -4,22 +4,11 @@ const handleOnMouseMove = e =>
     const rect = target.getBoundingClientRect(),
         x = e.clientX - rect.left,
         y = e.clientY - rect.top;
-        // w = rect.width,
-        // h = rect.height,
-        // browserZoomLevel = window.devicePixelRatio;
-        // r = Math.min(w, h) * 0.06;
     target.style.setProperty("--mouse-x", `${x}px`);
     target.style.setProperty("--mouse-y", `${y}px`);
-    // target.style.setProperty("border-radius", `${r}px`);
 }
 
 for(const card of document.querySelectorAll(".card, .wide-card"))
 {
     card.onmousemove = e => handleOnMouseMove(e);
-    // const rect = card.getBoundingClientRect(),
-    //     w = rect.width,
-    //     h = rect.height,
-    //     browserZoomLevel = window.devicePixelRatio;
-    //     r = Math.min(w, h) * 0.06;
-    // card.style.setProperty("border-radius", `${r}px`);
 }
